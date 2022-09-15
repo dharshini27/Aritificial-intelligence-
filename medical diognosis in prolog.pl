bcc807e6bce8f88ebbc50289d5e1e13ef910a22d
dharshini27 <112859031+dharshini27@users.noreply.github.com>
2022-09-15 08:59:07 +0530

    Add files via upload

---
+++ medical diognosis in prolog.pl
@@ -0,0 +1,43 @@
+/*
+domain
+   disease,indicates,name=symbol.
+predicates
+   hypothesis(name,disease),
+   symptom(name,indication).
+clauses
+*/
+   symptom(yamini,fever).
+   symptom(yamini,rash).
+   symptom(yamini,headache).
+   symptom(yamini,running_nose).
+   symptom(hemanth,chills).
+   symptom(hemanth,fever).
+   symptom(hemanth,headache).
+   symptom(radhika,running_nose).
+   symptom(radhika,rash).
+   symptom(radhika,flu).
+
+   hypothesis1(patent,measles).
+   symptom(patient,fever).
+   symptom(patient,cough).
+   symptom(patient,conjunctivitis).
+   symptom(patient,rash).
+
+   hypothesis2(patient,german_measles).
+   symptom(patient,fever).
+   symptom(patient,headache).
+   symptom(patient,running_nose).
+   symptom(patient,rash).
+
+
+
+
+
+
+
+
+
+
+
+
+
